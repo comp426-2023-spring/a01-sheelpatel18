511507e67931b2c3d38823571bb6659e46375e82
Sheel Patel <sheel@letswhirl.com>
2023-04-09 22:04:04 -0400

    new server.js

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 const port = args?.port || 3000; //  backfall to 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    // content type as html
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./public/index.html', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    })
-})
+fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+    http.createServer((req, res) => {
+        res.statusCode = 200;
+        // content type as html
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    }).listen(port, () => {
+        console.log(`Server running at port ${port}`);
+    })
+
 })
